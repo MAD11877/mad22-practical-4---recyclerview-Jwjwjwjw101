3c0f0bc092e77f454d730e30d01f27a974e09370
Jwjwjwjw101 <S10204864@connect.np.edu.sg>
2022-08-21 19:32:14 +0800

    //practical-4

---
+++ app/src/main/java/sg/edu/np/practical_4_jw/User.java
@@ -0,0 +1,15 @@
+package sg.edu.np.practical_4_jw;
+
+public class User {
+    public String name;
+    public String description;
+    public int id;
+    public boolean followed;
+    public User (String name, String description, int id, boolean followed)
+    {
+        name = name;
+        description = description;
+        id = id;
+        followed = followed;
+    }
+}
